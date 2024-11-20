90bd2864c2dc579ce52ab61f6825112c9a173a7a
lucascr <lcrodrigues1995@gmail.com>
2024-11-20 14:06:40 -0300

    Update tailwind.config.mjs

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,22 @@
+import defaultTheme from "tailwindcss/defaultTheme";
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     extend: {
+      fontFamily: {
+        bebas: ["Bebas Neue", ...defaultTheme.fontFamily.sans],
+        serif: ["Rokkitt", ...defaultTheme.fontFamily.serif],
+        stock: [
+          "ui-sans-serif",
+          "system-ui",
+          "sans-serif",
+          "Apple Color Emoji",
+          "Segoe UI Emoji",
+          "Segoe UI Symbol",
+          "Noto Color Emoji",
+        ],
+      },
       colors: {
         "main-black": "#231f20",
         "main-blue": "#00a3e4",
