a157f1f38f35f3aaf20a34da481166a7831f1606
lucascr <lcrodrigues1995@gmail.com>
2024-11-22 15:49:35 -0300

    Update CounterCard.jsx

--- src/components/CounterCard.jsx
+++ src/components/CounterCard.jsx
@@ -2,7 +2,7 @@ import CountUp from "react-countup";
 
 const Counter = ({ value, title }) => {
   return (
-    <div>
+    <nav>
       <div className="bg-dark-blue flex flex-col py-10 group">
         <div className="border-r border-white/20 px-10 flex flex-col items-center gap-1 group-last:border-0">
           <span className="text-7xl font-medium">
@@ -15,12 +15,12 @@ const Counter = ({ value, title }) => {
             />
             <span className="text-main-blue">+</span>
           </span>
-          <h2 className="text-main-black/80 text-lg text-center whitespace-nowrap">
+          <h2 className="text-lg text-center whitespace-nowrap">
             {title}
           </h2>
         </div>
       </div>
-    </div>
+    </nav>
   );
 };
 
