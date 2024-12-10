cde51961a09092b4b8509093f8774886ec93ad28
lucascr <lcrodrigues1995@gmail.com>
2024-12-10 16:59:51 -0300

    Update testimonialsSwiper.js

--- src/data/testimonialsSwiper.js
+++ src/data/testimonialsSwiper.js
@@ -1,45 +1,37 @@
 const testimonialsSwiper = {
-  upperHeading: "Testimoniales",
   heading: "Testimoniales",
   description:
     "Conoce la experiencia de nuestros estudiantes",
   cards: [
     {
-      title: "Private Pilot",
+      quoteText: `“iFly es la familia que me vió nacer como piloto aviador profesional y
+      me ayudó a cumplir uno de mis más grandes sueños, es para mí un placer
+      y un orgullo haberme formado en esta gran familia.”`,
+      citeText: `- Roberto Novelo Cicero (Piloto aviador)`,
       imagePath: "/src/assets/ifly-logo-white-no-text.png",
       imageAlt:
-        "Photograph of a UFT plane on runway",
-      description:
-        "Your passport to the skies! Learn to pilot your dreams with our Private Pilot Course.",
-      link: "/programs/private-pilot-course",
+        "iFly logo in white",
     },
     {
-      title: "Instrument Rating",
+      quoteText: `“Estudiar en iFly fue una de las mejores decisiones que he tomado en mi vida y una gran experiencia. Si me preguntaran de nuevo la elegiría sin pensarlo, ya que me permitió sentar bases sólidas en mi formación como piloto desde el punto de vista académico y personal. Cuentan con instalaciones de primer nivel, instructores altamente capacitados, aviones con excelente mantenimiento y una gran diversidad de características que hoy en día me ayudan a realizar mi trabajo de forma profesional y segura.`,
+      citeText: `- Eduardo Luján Aldaz (Piloto aviador)`,
       imagePath: "/src/assets/ifly-logo-white-no-text.png",
       imageAlt:
-        "Photograph of a UFT plane's assistive equipment",
-      description:
-        "Navigate through clouds and the skies with precision through our Instrument Rating course.",
-      link: "/programs/instrument-rating",
+        "iFly logo in white",
     },
     {
-      title: "Commercial Pilot",
+      quoteText: `“Estudié mi carrera de piloto comercial en iFly, y puedo decir que tienen excelentes instalaciones, aeronaves, simuladores. Tienen muy buena calidad en la enseñanza con muy buenos instructores.”`,
+      citeText: `- Rodrigo Minor (Piloto aviador)`,
       imagePath: "/src/assets/ifly-logo-white-no-text.png",
       imageAlt:
-        "Photograph of a UFT plane's propellers",
-      description:
-        "Start your pilot career with our Commercial Pilot Course. Your path to the captain's seat is here.",
-      link: "/programs/commercial-pilot-training",
+        "iFly logo in white",
     },
     {
-      title: "Multi-Engine Rating",
-      imagePath:
-        "/src/assets/ifly-logo-white-no-text.png",
+      quoteText: `“Haber estudiado en iFly sin duda alguna fue una excelente decisión, iFly es una institución que te provee las mejores herramientas para desarrollarte profesionalmente como piloto aviador comercial, hace que te rodees de gente con gran experiencia y completamente inmersa en el medio aeronáutico. Los instructores de la escuela además de ser excelentes personas, son gente completamente apasionada por este hermoso medio y mas importante personas con ganas de promover y mejorar el medio aeronáutico en México. Con toda seguridad puedo decir que es la mejor escuela de aviación en México.”`,
+      citeText: `- Luis Miguel Pérez Villalón (Piloto aviador)`,
+      imagePath: "/src/assets/ifly-logo-white-no-text.png",
       imageAlt:
-        "UFT's multi-engine aircraft, blue skies",
-      description:
-        "Master the art of multi engine flying and unlock new horizons in your aviation career.",
-      link: "/programs/multi-engine-rating",
+        "iFly logo in white",
     },
   ],
 };
