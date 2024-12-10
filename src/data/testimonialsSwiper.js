8b3cc82a9a6972ad333c43f72ea726ac00a61060
lucascr <lcrodrigues1995@gmail.com>
2024-12-10 14:05:22 -0300

    Create testimonialsSwiper.js

---
+++ src/data/testimonialsSwiper.js
@@ -0,0 +1,47 @@
+const testimonialsSwiper = {
+  upperHeading: "Testimoniales",
+  heading: "Testimoniales",
+  description:
+    "Conoce la experiencia de nuestros estudiantes",
+  cards: [
+    {
+      title: "Private Pilot",
+      imagePath: "/src/assets/ifly-logo-white-no-text.png",
+      imageAlt:
+        "Photograph of a UFT plane on runway",
+      description:
+        "Your passport to the skies! Learn to pilot your dreams with our Private Pilot Course.",
+      link: "/programs/private-pilot-course",
+    },
+    {
+      title: "Instrument Rating",
+      imagePath: "/src/assets/ifly-logo-white-no-text.png",
+      imageAlt:
+        "Photograph of a UFT plane's assistive equipment",
+      description:
+        "Navigate through clouds and the skies with precision through our Instrument Rating course.",
+      link: "/programs/instrument-rating",
+    },
+    {
+      title: "Commercial Pilot",
+      imagePath: "/src/assets/ifly-logo-white-no-text.png",
+      imageAlt:
+        "Photograph of a UFT plane's propellers",
+      description:
+        "Start your pilot career with our Commercial Pilot Course. Your path to the captain's seat is here.",
+      link: "/programs/commercial-pilot-training",
+    },
+    {
+      title: "Multi-Engine Rating",
+      imagePath:
+        "/src/assets/ifly-logo-white-no-text.png",
+      imageAlt:
+        "UFT's multi-engine aircraft, blue skies",
+      description:
+        "Master the art of multi engine flying and unlock new horizons in your aviation career.",
+      link: "/programs/multi-engine-rating",
+    },
+  ],
+};
+
+export default testimonialsSwiper;
