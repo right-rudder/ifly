7355ff0d616bb94db8c46395c46005496868c5ef
lucascr <lcrodrigues1995@gmail.com>
2024-12-11 17:11:10 -0300

    Update aviationManagement.js

--- src/data/aviationManagement.js
+++ src/data/aviationManagement.js
@@ -19,16 +19,18 @@ const aviationManagement = {
     headerAlt: "Piloto frente a un Cessna",
     primaryImage: "/src/assets/piloto-en-cabina-de-avion-ifly.webp",
     primaryAlt: "Piloto en cabina de avión de iFly",
+    perfil: `Egresados competentes en la toma de decisiones de la organización, operación y logística de las aerolíneas y aeropuertos con conocimientos de las áreas administrativas, operacionales, técnicas y logísticas. Logrando desempeñarse como administrador de negocios de transporte aéreo, comercio aéreo, así como también planificación de aeropuertos.
+            <br/><br/>Los egresados también se formarán como pilotos aviadores de ala fija profesionales que se guían bajo la normatividad y procedimientos para operar una aeronave de manera eficiente y segura con fines privados o comerciales de acuerdo con los reglamentos oficiales vigentes.`,
     prerequisites: [
-      "Ser mexicano/a",
-      "Mayor de 18 años",
-      "Comprobante de domicilio",
-      "Acta de nacimiento",
-      "CURP",
-      "INE / Pasaporte (vigente)",
-      "Certificado de estudios de bachillerato",
-      "6 fotografías tamaño infantil a color y 6 fotografías tamaño pasaporte a color (con camisa blanca y corbata negra)",
-      "<a href='http://www.sct.gob.mx/index.php?id=2306' target='_blank' class='hover:text-main-blue duration-300'>Constancia de aptitud psicofísica grupo I expedida por la Dirección General de Protección y Medicina Preventiva del Transporte (SCT)</a>",
+      "Ser mexicano/a.",
+      "Mayor de 18 años.",
+      "Comprobante de domicilio.",
+      "Acta de nacimiento.",
+      "CURP.",
+      "INE / Pasaporte (vigente).",
+      "Certificado de estudios de bachillerato.",
+      "6 fotografías tamaño infantil a color y 6 fotografías tamaño pasaporte a color (con camisa blanca y corbata negra).",
+      "<a href='http://www.sct.gob.mx/index.php?id=2306' target='_blank' class='hover:text-main-blue duration-300'>Constancia de aptitud psicofísica grupo I expedida por la Dirección General de Protección y Medicina Preventiva del Transporte (SCT).</a>",
     ],
     faqs: [
       {
