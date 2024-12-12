557130e649085beae42bf28f99bdf21c1602cba1
lucascr <lcrodrigues1995@gmail.com>
2024-12-11 23:42:09 -0300

    create cursos complementarios page

---
+++ src/data/cursosComplementares.js
@@ -0,0 +1,31 @@
+const cursosComplementares = {
+  data: {
+    title: "Cursos Complementarios",
+    url: "/carreras/cursos-complementarios",
+    slug: "cursos-complementarios",
+    siteTitle:
+      "Cursos Complementarios - iFly Universidad Aeronáutica",
+    siteDescription:
+      "iFly, la primera universidad de aeronáutica para pilotos aviadores en México, que te ofrece una formidable preparación para cumplir el sueño de volar profesionalmente aeronaves de distinto tipo.",
+    headerImage:
+      "cursos-complementarios-banner",
+    headerAlt: "Técnico de mantenimiento arreglando un Cessna",
+    primaryImage:
+      "/src/assets/tecnicos-de-mantenimiento-empujando-avion-hacia-el-hangar.jpg",
+    primaryAlt: "Técnicos de mantenimiento empujando avión hacia el hangar",
+    cursosH: "CURSOS COMPLEMENTARIOS",
+    prerequisites: [
+      "Recuperación y revalidación de licencia",
+      "Convalidación de licencia",
+      "Curso CFIT/ALAR/CRM",
+      "Curso periódico de oficial de operaciones",
+      "Inicial y periódico de mantenimiento de mecánicos:",
+      "Técnicas didácticas",
+      "Formación de instructores",
+      "Mercancías peligrosas por vía aérea",
+      "Capacidad de multimotor e instrumentos",
+    ],
+  },
+};
+
+export default cursosComplementares;
